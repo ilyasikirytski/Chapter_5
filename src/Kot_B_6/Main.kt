da83aceb269aee74740e46c6e280d388bb76c72a
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-03-24 14:40:12 +0300

    B_6 scope fun add in main

--- src/Kot_B_6/Main.kt
+++ src/Kot_B_6/Main.kt
@@ -4,10 +4,12 @@ fun main() {
     val kotCatalog = KotCatalog()
     val book1 = kotCatalog.KotBook(KotBookName.FIRST)
     val book2 = kotCatalog.KotBook(KotBookName.SECOND)
-    kotCatalog.addBooks(book1, book2)
-    kotCatalog.takeBook(book1, "1")
-    kotCatalog.returnBook(book1)
-    kotCatalog.takeBook(book1, "2")
-    kotCatalog.takeBook(book1, "3")
-    kotCatalog.printRequestHistory(book1)
+    kotCatalog.let {
+        it.addBooks(book1, book2)
+        it.takeBook(book1, "1")
+        it.returnBook(book1)
+        it.takeBook(book1, "2")
+        it.takeBook(book1, "3")
+        it.printRequestHistory(book1)
+    }
 }
